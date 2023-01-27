2ab2571d8fb25a9792845da8143cb73abe4b439d
Vincent <VincentNSWE@gmail.com>
2023-01-27 13:50:54 +0800

    task02 semi

--- task02/src/sdf/Constants.java
+++ task02/src/sdf/Constants.java
@@ -1,12 +1,12 @@
-package sdf;
+// package sdf;
 
-public class Constants {
+// public class Constants {
 
-    // The operators and also exit 
-    // public static final String ADD =        "+";
-    // public static final String SUBTRACT =   "-";
-    // public static final String DIVIDE =     "/";
-    // public static final String MULTIPLY =   "*";
-    public static final String EXIT = "exit";
+//     The operators and also exit 
+//     public static final String ADD =        "+";
+//     public static final String SUBTRACT =   "-";
+//     public static final String DIVIDE =     "/";
+//     public static final String MULTIPLY =   "*";
+//     public static final String EXIT = "exit";
 
-}
+// }
